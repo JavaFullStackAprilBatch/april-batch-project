bc3c26e05c43bab7e732056ad08ffdb39fcc9cd8
Raja <kasani.sekhar@gmail.com>
2024-06-11 23:27:41 -0400

    POST method to create new batch.

--- src/main/java/com/example/aprilbatchproject/controller/BtachController.java
+++ src/main/java/com/example/aprilbatchproject/controller/BtachController.java
@@ -1,20 +1,30 @@
 package com.example.aprilbatchproject.controller;
 
 
+import com.example.aprilbatchproject.dto.BatchDTO;
 import com.example.aprilbatchproject.entity.Batches;
+import com.example.aprilbatchproject.response.ApiResponse;
 import com.example.aprilbatchproject.service.BatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/batches")
+@RequestMapping("/registration")
 public class BtachController {
 
     @Autowired
     BatchService batchService;
     @PostMapping("/newStudent")
-    public String createNewBatch(@RequestBody Batches batches){
+    public String createNewStudentBatch(@RequestBody Batches batches){
         batchService.createStudentsBatch(batches);
         return "New Student Data Save Successfully";
+    }
+
+    @PostMapping("/newBatch")
+    public ResponseEntity<ApiResponse<BatchDTO>> createNewBatch(@RequestBody BatchDTO dto){
+        BatchDTO createNewBatch = batchService.createBatch(dto);
+        return new ResponseEntity<>(new ApiResponse<>(true, "Student created successfully",createNewBatch ), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/aprilbatchproject/service/BatchService.java
+++ src/main/java/com/example/aprilbatchproject/service/BatchService.java
@@ -1,8 +1,13 @@
 package com.example.aprilbatchproject.service;
 
 
+import com.example.aprilbatchproject.dto.BatchDTO;
 import com.example.aprilbatchproject.entity.Batches;
+import com.example.aprilbatchproject.entity.Courses;
+import com.example.aprilbatchproject.entity.Trainers;
 import com.example.aprilbatchproject.repository.BatchRepository;
+import com.example.aprilbatchproject.repository.CourseRepository;
+import com.example.aprilbatchproject.repository.TrainerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +15,40 @@ import org.springframework.stereotype.Service;
 public class BatchService {
     @Autowired
     BatchRepository batchRepository;
+    @Autowired
+    TrainerRepository trainerRepository;
+    @Autowired
+    CourseRepository courseRepository;
     public String createStudentsBatch(Batches batches){
         batchRepository.save(batches);
         return "Data saved";
     }
+
+    public BatchDTO createBatch (BatchDTO dto) {
+        Trainers trainer = null;
+        Courses course = null;
+        try{
+             trainer = trainerRepository.getTrainerByName(dto.getTrainerName());
+             course = courseRepository.getCourseByName(dto.getCourseName());
+            Batches batch = new Batches();
+            batch.setBatch_name(dto.getBatchName());
+            batch.setStart_date(dto.getBatchStart());
+            batch.setEnd_date(dto.getBatchEnd());
+            batch.setCourses(course);
+            batch.setTrainer(trainer);
+
+            Batches saveBatch =  batchRepository.save(batch);
+        }catch (Exception e){
+            if(trainer == null) {
+                throw new IllegalArgumentException("Trainer Name Not Found");
+            }
+            if(course == null){
+                throw new IllegalArgumentException("Course Name Not Found");
+            }
+        }
+
+        return dto;
+    }
+
+
 }
