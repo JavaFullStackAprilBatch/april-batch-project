b5f0e5116f935c776f863478c7c1b2fb0067cef5
Raja <kasani.sekhar@gmail.com>
2024-06-21 19:13:20 -0400

    Get ongoing batches method.

--- src/main/java/com/example/aprilbatchproject/controller/BtachController.java
+++ src/main/java/com/example/aprilbatchproject/controller/BtachController.java
@@ -2,16 +2,21 @@ package com.example.aprilbatchproject.controller;
 
 
 import com.example.aprilbatchproject.dto.BatchDTO;
+import com.example.aprilbatchproject.dto.CourseDTO;
 import com.example.aprilbatchproject.entity.Batches;
+import com.example.aprilbatchproject.entity.StatusType;
 import com.example.aprilbatchproject.response.ApiResponse;
 import com.example.aprilbatchproject.service.BatchService;
+import org.hibernate.engine.jdbc.batch.spi.Batch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/registration")
+@RequestMapping("/batch")
 public class BtachController {
 
     @Autowired
@@ -22,9 +27,16 @@ public class BtachController {
         return "New Student Data Save Successfully";
     }
 
-    @PostMapping("/newBatch")
+    @PostMapping("/registration/newBatch")
     public ResponseEntity<ApiResponse<BatchDTO>> createNewBatch(@RequestBody BatchDTO dto){
         BatchDTO createNewBatch = batchService.createBatch(dto);
         return new ResponseEntity<>(new ApiResponse<>(true, "New Batch created successfully",createNewBatch ), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<ApiResponse<List<BatchDTO>>> retrieveOngingBatchs(@RequestParam String status){
+       List<BatchDTO> batches =batchService.getOngoingBatchs(status);
+        ApiResponse<List<BatchDTO>> response = new ApiResponse<>(true, "Batches fetched successfully", batches);
+        return  ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/aprilbatchproject/util/DataConverter.java
+++ src/main/java/com/example/aprilbatchproject/util/DataConverter.java
@@ -1,7 +1,13 @@
 package com.example.aprilbatchproject.util;
 
+import com.example.aprilbatchproject.dto.BatchDTO;
 import com.example.aprilbatchproject.dto.CourseDTO;
 import com.example.aprilbatchproject.dto.TrainerDTO;
+import com.example.aprilbatchproject.entity.Batches;
+import com.example.aprilbatchproject.entity.Courses;
+import com.example.aprilbatchproject.entity.Trainers;
+import com.example.aprilbatchproject.exception.ResourceNotFoundException;
+import org.hibernate.engine.jdbc.batch.spi.Batch;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +26,30 @@ public class DataConverter {
             trainerDTOs.add(new TrainerDTO(courseName));
         }
         return trainerDTOs;
+    }
+
+    public static   List<BatchDTO> convertToBatchDTOs(List<Batches> batches){
+        List<BatchDTO> batchDTOS = new ArrayList<>();
+        for(Batches batch : batches){
+            BatchDTO batchDTO = new BatchDTO();
+            batchDTO.setBatchName(batch.getBatch_name());
+            batchDTO.setBatchStart(batch.getStart_date());
+            batchDTO.setBatchEnd(batch.getEnd_date());
+            Courses courses = batch.getCourses();
+            if(courses !=null){
+                batchDTO.setCourseName(courses.getCourse_name());
+            }else {
+                throw new ResourceNotFoundException("CourseName Not Found");
+            }
+            Trainers trainers = batch.getTrainer();
+            if(trainers != null){
+                batchDTO.setTrainerName(trainers.getName());
+            }else {
+                throw new ResourceNotFoundException("TrainerName Not Found");
+            }
+            batchDTO.setBatchStatus(batch.getStatus());
+            batchDTOS.add(batchDTO);
+        }
+        return batchDTOS;
     }
 }
