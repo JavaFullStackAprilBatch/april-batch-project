5816736d49d1f667fd78af91fe7ba6f334b620b5
Raja <kasani.sekhar@gmail.com>
2024-06-23 20:46:56 -0400

    Batch name is unique.

--- src/main/java/com/example/aprilbatchproject/service/BatchService.java
+++ src/main/java/com/example/aprilbatchproject/service/BatchService.java
@@ -5,6 +5,7 @@ import com.example.aprilbatchproject.dto.BatchDTO;
 import com.example.aprilbatchproject.entity.Batches;
 import com.example.aprilbatchproject.entity.Courses;
 import com.example.aprilbatchproject.entity.Trainers;
+import com.example.aprilbatchproject.exception.DuplicateResourceFoundException;
 import com.example.aprilbatchproject.exception.ResourceNotFoundException;
 import com.example.aprilbatchproject.repository.BatchRepository;
 import com.example.aprilbatchproject.repository.CourseRepository;
@@ -28,17 +29,23 @@ public class BatchService {
     public BatchDTO createBatch (BatchDTO dto) {
         Trainers trainer = null;
         Courses course = null;
-        Batches batch = new Batches();
+        Batches newBatch = new Batches();
         try {
             trainer = trainerRepository.getTrainerByName(dto.getTrainerName());
             course = courseRepository.getCourseByName(dto.getCourseName());
+            Batches batch = batchRepository.findByBatchName(dto.getBatchName());
 
-            batch.setBatch_name(dto.getBatchName());
-            batch.setStart_date(dto.getBatchStart());
-            batch.setEnd_date(dto.getBatchEnd());
-            batch.setCourses(course);
-            batch.setTrainer(trainer);
-            batch.setStatus(dto.getBatchStatus());
+            if(batch == null){
+                newBatch.setBatch_name(dto.getBatchName());
+                newBatch.setStart_date(dto.getBatchStart());
+                newBatch.setEnd_date(dto.getBatchEnd());
+                newBatch.setCourses(course);
+                newBatch.setTrainer(trainer);
+                newBatch.setStatus(dto.getBatchStatus());
+            }else {
+                throw new DuplicateResourceFoundException("Batch name already exist, can not create duplicate batch name");
+            }
+
 
         }catch (Exception e) {
             throw new IllegalArgumentException(e);
@@ -52,7 +59,7 @@ public class BatchService {
             throw new ResourceNotFoundException("Course Not Found");
         }
 
-        Batches saveBatch =  batchRepository.save(batch);
+        Batches saveBatch =  batchRepository.save(newBatch);
 
         return dto;
     }
